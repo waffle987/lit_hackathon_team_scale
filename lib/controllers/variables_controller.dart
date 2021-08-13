3f07a607777e26e9685e7e493f58f481116c6dff
ChuangZheQuan <zhequan7chuang@gmail.com>
2021-08-13 18:02:43 +0800

    added getx and firebase to pubspec

---
+++ lib/controllers/variables_controller.dart
@@ -0,0 +1,22 @@
+import 'package:get/get.dart';
+
+class varController extends GetxController {
+  List<String> findVars(String block) {
+    RegExp exp = RegExp(r"{{([(a-zA-Z)])\w+}}");
+    List<String> listOfVars = [];
+    Iterable<Match> matches = exp.allMatches(block);
+    for (Match m in matches) {
+      String match = m[0]!;
+      listOfVars.add(match);
+    }
+    return listOfVars;
+  }
+
+  replaceVars(List<String> replacements, String block) {
+    for (String word in findVars(block)) {
+      int index = findVars(block).indexOf(word);
+      RegExp exp = RegExp(word, caseSensitive: false);
+      block = block.replaceFirst(exp, replacements[index]);
+    }
+  }
+}
