c4c4a5e06a484d7cd6639934bb3682b22ffe6d94
ChuangZheQuan <zhequan7chuang@gmail.com>
2021-08-14 09:17:11 +0800

    wat

--- lib/ui/home/home_page.dart
+++ lib/ui/home/home_page.dart
@@ -8,13 +8,10 @@ import 'package:lit_hackathon_team_scale/widgets/list_view_variables.dart';
 import 'package:lit_hackathon_team_scale/controllers/var_change_controller.dart';
 import 'package:responsive_builder/responsive_builder.dart';
 import 'package:get/get.dart';
-import 'package:flutter_quill/flutter_quill.dart' hide Text;
-import 'package:flutter_markdown/flutter_markdown.dart';
 
 class HomePage extends StatelessWidget {
   HomePage({Key? key}) : super(key: key); //can add back the const later
   final VarChangeController c = Get.put(VarChangeController());
-  QuillController _controller = QuillController.basic();
 
   @override
   Widget build(BuildContext context) {
@@ -26,25 +23,8 @@ class HomePage extends StatelessWidget {
         body: CenteredView(
           child: Column(
             children: [
-              Obx(() => Expanded(child: Markdown(data: c.text.value))),
               Obx(() => Text(c.text.value)),
               Obx(() => ListViewVariables(c.findVars(c.text.value))),
-              QuillToolbar.basic(controller: _controller),
-              Expanded(
-                child: Container(
-                  child: QuillEditor.basic(
-                    controller: _controller,
-                    readOnly: false, // true for view only mode
-                  ),
-                ),
-              ),
-              ElevatedButton(
-                  child: const Text('Submit'),
-                  onPressed: () {
-                    // update the text;
-                    print(c.results.value.toString());
-                    c.setText(_controller.document.toDelta().toString());
-                  }),
               NavigationBar(),
               Expanded(
                 child: ScreenTypeLayout(
