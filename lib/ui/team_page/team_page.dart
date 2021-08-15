54307ff88c9ccd8c937e53366a6ac7a2beeab8a3
Yap Jia Jun <yapx08@hotmail.com>
2021-08-15 09:25:16 +0800

    RoboDoc Team UI Update

--- lib/ui/team_page/team_page.dart
+++ lib/ui/team_page/team_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lit_hackathon_team_scale/config/ui_helpers.dart';
 import 'package:lit_hackathon_team_scale/widgets/centred_view.dart';
 
 class TeamPage extends StatelessWidget {
@@ -13,44 +14,56 @@ class TeamPage extends StatelessWidget {
       required String bio,
       required String photo,
     }) {
-      return Container(
-        child: Card(
-          margin: EdgeInsets.all(10),
-          clipBehavior: Clip.antiAlias,
-          shape:
-              RoundedRectangleBorder(borderRadius: BorderRadius.circular(15.0)),
-          child: Stack(
-            alignment: Alignment.bottomLeft,
-            children: [
-              Container(
-                child: Image.asset(photo),
-              ),
-              Container(
-                color: Colors.white,
-                height: _mediaQuery.size.height * 0.125,
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        name,
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontWeight: FontWeight.bold,
-                        ),
-                        overflow: TextOverflow.ellipsis,
-                      ),
-                      Text(
-                        bio,
-                        overflow: TextOverflow.ellipsis,
-                        textAlign: TextAlign.center,
-                      ),
-                    ],
+      return Padding(
+        padding: const EdgeInsets.all(3.0),
+        child: Container(
+          height: 100,
+          child: Card(
+            margin: EdgeInsets.all(1),
+            clipBehavior: Clip.antiAlias,
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(15.0)),
+            child: Stack(
+              alignment: Alignment.bottomLeft,
+              children: [
+                Positioned.fill(
+                  child: Container(
+                    child: Image.asset(photo,
+                    fit: BoxFit.fill,),
                   ),
                 ),
-              )
-            ],
+                Container(
+                  color: Colors.white,
+                  height: _mediaQuery.size.height * 0.1,
+                  child: Padding(
+                    padding: const EdgeInsets.all(5.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          name,
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 10,
+                          ),
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                        verticalSpaceSmall,
+                        Text(
+                          bio,
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 8,
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       );
@@ -64,26 +77,26 @@ class TeamPage extends StatelessWidget {
         child: GridView(
           shrinkWrap: true,
           gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
-            mainAxisSpacing: 20.0,
-            crossAxisSpacing: 100.0,
-            childAspectRatio: 1,
+            crossAxisCount: 4,
+            mainAxisSpacing: 3.0,
+            crossAxisSpacing: 10.0,
+            childAspectRatio: 0.55,
           ),
           children: [
             _buildCard(
                 name: 'Zhe Quan',
                 bio:
-                    'Hi                                                                   ',
+                    'A student that loves to code and wants to contribute back to society through the varse knowledge he gained.                                                                    ',
                 photo: 'assets/JJ.jpg'),
             _buildCard(
                 name: 'Darren Ong',
                 bio:
-                    'Loves to explore the legal needs of companies from all perspectives.  Has experience in-house at a listed company and a start-up, as well as in private law firms. From these, he believes that tech is the key to improving legal processes,  simultaneously reducing time and costs whilst increasing accuracy and reliability. In his spare time, he loves hunting down good food and volunteering with People with Intellectual Disabilities',
+                    'Loves to explore the legal needs of companies from all perspectives.  Has experience in-house at a listed company and a start-up, as well as in private law firms. From these, he believes that tech is the key to improving legal processes.',
                 photo: 'assets/Darren.jpg'),
             _buildCard(
                 name: 'Jia Jun',
                 bio:
-                    'Jia Jun is a incoming freshman from the Nanyang Technological University. His passion for coding started during his National Service days, where he learned and picked up numerous coding languages. During his free time, he loves running and is also a avid traveller/hiker, exploring different parts of Singapore by foot and cycling.',
+                    'Jia Jun is a incoming freshman from the Nanyang Technological University. His passion for coding started during his National Service days, where he learned and picked up numerous coding languages. ',
                 photo: 'assets/JJ.jpg'),
             _buildCard(
                 name: 'Ben',
